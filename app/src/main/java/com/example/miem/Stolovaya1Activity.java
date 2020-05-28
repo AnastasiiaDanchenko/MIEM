d63c0fef354b9695c194333d5e73766c353cdb73
AnastasiiaDanchenko <anas4enko@yandex.ru>
2020-05-28 21:30:04 +0300

    Update Stolovaya1Activity.java

--- app/src/main/java/com/example/miem/Stolovaya1Activity.java
+++ app/src/main/java/com/example/miem/Stolovaya1Activity.java
@@ -1,13 +1,16 @@
 package com.example.miem;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,10 +28,19 @@ public class Stolovaya1Activity extends AppCompatActivity {
         final MediaPlayer paperSound = MediaPlayer.create(this, R.raw.paper);
         final MediaPlayer lestnitsSound = MediaPlayer.create(this, R.raw.lestnitsa);
 
+        SharedPreferences s_level = getSharedPreferences("levelSettings", MODE_PRIVATE);
+        final int level = s_level.getInt("LEVEL", 1);
+
         numbersButton = findViewById(R.id.numbers_button);
         lestnitsaButton = findViewById(R.id.lestnitsa_button);
         stolovayaButton = findViewById(R.id.stolovaya_button);
         pauseButton = findViewById(R.id.pause_button);
+
+        if (level<7){
+            Toast toast = Toast.makeText(getApplicationContext(), "Что это там за графики?..", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        }
 
         pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
